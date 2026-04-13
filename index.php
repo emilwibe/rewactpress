8e0619575b6637ceb2d3d75c35e2367807179ce8
Benjamin Hingebjerg <benjaminhingebjerg@protonmail.com>
2026-04-13 10:13:46 +0200

    Added comments to index.php and footer.php

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
-<?php get_header(); ?>
+<?php 
 
+/** Loads the theme header template (header.php) */
+get_header(); 
+
+?>
+
+<!-- React app will mount here -->
 <div id="root"></div>
 
-<?php get_footer(); ?>
+<?php 
+
+/** Loads the theme footer template (footer.php) */
+get_footer(); 
+
+?>
